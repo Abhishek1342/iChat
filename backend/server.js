235cceebf647574284277ffc6f4a497bfdf02f06
Abhishek kumar <63067796+Abhishek1342@users.noreply.github.com>
2021-08-19 23:32:19 +0530

    Db setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const env = require("dotenv");
+const path = require("path");
+
+env.config({ path: path.resolve(__dirname, "./.env") });
+require("./DB/DBConnection");
 
 app.get("/", (req, res) => {
     res.send("Ichat");
