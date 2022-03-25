bd1ba6c90cff52098925c3f6efc1021f58bec427
Abhishek kumar <63067796+Abhishek1342@users.noreply.github.com>
2022-03-25 20:58:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const Socket = require("./Socket");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.options('*',cors())
+app.use("*",cors());
 
 app.use("/Images", express.static(path.join("Images")));
 env.config({ path: path.resolve(__dirname, "./.env") });
@@ -24,9 +25,7 @@ const storage = multer.diskStorage({
 
 app.use(multer({ storage: storage }).single("profileImage"));
 
-app.options('*',cors())
 app.use("/api/", require("./Routes/account"));
-app.options('*',cors())
 app.use("/api/", require("./Routes/message"));
 
 app.use((req, res, next) => {
